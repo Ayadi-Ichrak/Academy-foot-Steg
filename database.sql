7906128fb1b9805ea57a6fdf42339b35a0a90136
Ayadi-Ichrak <ichrakayadi24@gmail.com>
2024-10-18 17:25:36 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -139,44 +139,4 @@ ALTER TABLE Planning
 ADD CONSTRAINT FK_Planning_Groupe FOREIGN KEY (groupe_id) REFERENCES Groupe(id),
 ADD CONSTRAINT FK_Planning_Terrain FOREIGN KEY (terrain_id) REFERENCES Terrain(id);
 
-INSERT INTO Joueur (code, nom, prenom, date_naissance, photo, groupe_id, parent_cin) VALUES
-('ABD070NN85', 'Nouira', 'Abdelkader', '1985-07-15', 'abdelkader.jpg', 1, 01234567),
-('SOU040DN90', 'Daghbaji', 'Sofiene', '1990-04-22', 'sofiene.jpg', 2, 12345678);
-
-INSERT INTO Parents (cin, nom, prenom, num_tel, photo, joueur_code) VALUES
-(01234567, 'Nouira', 'Habib', '98989898', 'habib.jpg', 'ABD070NN85'),
-(12345678, 'Daghbaji', 'Ali', '97979797', 'ali.jpg', 'SOU040DN90');
-
-INSERT INTO Entraineur (cin, nom, prenom, num_tel, date_cin) VALUES
-(01122334, 'Jaziri', 'Mohamed', 99778899,'1985-07-15'),
-(10111213, 'Trabelsi', 'Sami', 99887766,'1985-04-16');
-
-INSERT INTO Groupe (nom ) VALUES
-('2009-2010'),
-('2011-2012');
-
-INSERT INTO Terrain (nom, localisation) VALUES
-('Stade El Menzah', 'Tunis'),
-('Stade Hammamet', 'Hammamet');
-
-INSERT INTO Materiel (nom, quantite, date_achat) VALUES
-('Ballons', 20, '2024-01-15'),
-('Maillots', 30, '2024-02-10');
-
-INSERT INTO Paiement_Joueur (code, nom, prenom, montant, date_debut, date_fin, nb_mois) VALUES
-('BEN050AA92', 'Ben Salah', 'Ali', 450.00, '2024-07-01', DATE_ADD('2024-07-01', INTERVAL 3 MONTH), 3),
-('HAM030BB88', 'Hammami', 'Bilel', 600.00, '2024-06-15', DATE_ADD('2024-06-15', INTERVAL 4 MONTH), 4);
-
-INSERT INTO Paiement_Entraineur (cin, date_cin, nom, prenom, montant, date_debut, date_fin, nb_mois) VALUES
-(01122334, '2002-07-09','Nom1', 'Prenom1', 1000.00, '2024-07-05', DATE_ADD('2024-07-05', INTERVAL 1 MONTH), 1),
-(10111213, '2004-03-05','Nom2', 'Prenom2', 1200.00, '2024-06-28', DATE_ADD('2024-06-28', INTERVAL 1 MONTH), 1);
-
-INSERT INTO Planning (jour, heure,entraineur_cin, groupe_id, terrain_id) VALUES
-('2024-08-10', '10:00:00',01122334, 1, 1),
-('2024-08-11', '15:00:00',10111213, 2, 2);
-
-INSERT INTO users (identifiant, pass, user_type) VALUES
-('admin', 'admin', 'Admin'),
-('gestionnaire', 'gestionnaire', 'Gestionnaire'),
-('membre', 'membre', 'Membre');
 
